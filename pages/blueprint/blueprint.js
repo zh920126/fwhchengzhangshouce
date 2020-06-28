1f4fdd63066190b2a9f4547ef524e2f6e78295a1
zh920126 <308413147@qq.com>
2020-06-28 15:58:41 +0800

    人生蓝图2完成

--- pages/blueprint/blueprint.js
+++ pages/blueprint/blueprint.js
@@ -357,6 +357,40 @@ Page({
     completionTime: 3
   },
 
+  // 蓝图2数据更新
+  async handleUpdate5(e) {
+    let {array}=this.data
+    // 从event当中获取参数
+    let {index} = e.currentTarget.dataset
+    let {value} = e.detail
+    let data = {
+      completionTime: 5,
+      type: array[index].type,
+      content: value,
+      userid: wx.getStorageSync('userID')
+    }
+    if (array[index].id) {
+      data.id = array[index].id
+    }
+    let res = await app.myAxios({
+      method: 'post',
+      url: '/anonymous/updateBlueprint',
+      data
+    })
+    // console.log(res)
+    // 更新成功之后，需要重新获取数据进行页面渲染
+    if (res.data.statusCode == 200) {
+      this.getUserBlPrint2()
+    } else {
+      uni.showToast({
+        title: '更新失败,请重试',
+        duration: 500,
+        icon: 'none'
+      })
+    }
+  },
+
+
   // 蓝图1数据更新
   async handleUpdate3(e) {
     // 从event当中获取参数
@@ -372,13 +406,11 @@ Page({
     if (arr[index].id) {
       data.id = arr[index].id
     }
-    console.log(data)
     let res = await app.myAxios({
       method: 'post',
       url: '/anonymous/updateBlueprint',
       data
     })
-    console.log(res)
     // 更新成功之后，需要重新获取数据进行页面渲染
     if (res.data.statusCode == 200) {
       this.getUserBlPrint()
@@ -391,7 +423,87 @@ Page({
     }
   },
 
-  // 进入页面就获取蓝图1的数据
+  // 获取蓝图2的数据
+  async getUserBlPrint2() {
+    let data = {
+      completionTime: 5,
+      userid: wx.getStorageSync('userID')
+    }
+    let res = await app.myAxios({
+      method: 'post',
+      url: '/anonymous/queryBlueprint',
+      data
+    })
+    if (res.data.message == '查询成功' && res.data.result.content) {
+      let userBluePrint = res.data.result.content
+      let {array}=this.data
+      // 创建对象进行数据筛选
+      userBluePrint.forEach(v => {
+        if (v.type == '事业') {
+          array[0] = v
+        }
+        if (v.type == '财富') {
+          array[1] = v
+        }
+        if (v.type == '家庭生活') {
+          array[2] = v
+        }
+        if (v.type == '学习成长') {
+          array[3] = v
+        }
+        if (v.type == '人际关系') {
+          array[4] = v
+        }
+        if (v.type == '健康计划') {
+          array[5] = v
+        }
+      })
+      this.setData({
+        array
+      })
+    } else {
+      wx.showToast({
+        title: '获取人生蓝图2数据失败,请重试',
+        duration: 500,
+        icon: 'none'
+      })
+    }
+  },
+
+  // 获取蓝图3的数据内容
+  async viewBlPrint3(){
+    let res=await app.myAxios({
+      method:'post',
+      url:'/anonymous/queryBlueprintDetailed',
+      data:{
+        userid:wx.getStorageSync('userID')
+      }
+    })
+    // console.log(res);
+    // 更新视图
+    if(res.statusCode===200&&res.data.result.content){
+      let {content}=res.data.result
+      let {cateList}=this.data
+      for(let i=0;i<cateList.length;i++){
+        content.forEach(v=>{
+          if(cateList[i].name===v.parentType){
+            cateList[i]['children'][v.count-1]=v
+          }
+        })
+      }
+      this.setData({
+        cateList
+      })
+    }else{
+      wx.showToast({
+        title:'获取蓝图3数据失败,请重试',
+        icon:'none',
+        duration:500
+      })
+    }
+  },
+
+  // 获取蓝图1的数据
   async getUserBlPrint() {
     let data = {
       completionTime: 3,
@@ -436,6 +548,24 @@ Page({
         icon: 'none'
       })
     }
+  },
+
+  // tab栏切换
+  async handleClick(e) {
+    console.log(e)
+    let {index} = e.currentTarget.dataset
+    console.log(index);
+    //判断用户点击的是哪一项
+    if (index == 0) {
+      this.getUserBlPrint()
+    } else if (index == 1) {
+      this.getUserBlPrint2()
+    } else {
+      this.viewBlPrint3()
+    }
+    this.setData({
+      changeIndex:index
+    })
   },
   onLoad: function (options) {
     this.getUserBlPrint()
